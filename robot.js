c95938c543a797722abd896219bda56a16af5bb7
alicelara <hello@alicelara.ca>
2013-06-20 18:33:07 -0400

    jquery events added for graphic engagement

--- robot.js
+++ robot.js
@@ -1,10 +1,69 @@
 $(document).ready (function() {
-	$(document).keypress(function(e) {
-		$('#robot').append().text(e.type + ':' + e.which);
+	$(document).keydown(function(e) {
 
+		// listens for any navigation keypress activity
+			
+				switch(e.which) {
+					// user presses the "Q" STRAFE LEFT
+					case 81: 	$(".Q").addClass("active");
+						$(document).keyup(function(e){
+						$(".Q").removeClass("active");
+						});
+						
+						break; 
+								
+					// user presses the "W" key FORWARD
+					case 87:	$(".W").addClass("active");
+						$(document).keyup(function(e){
+						$(".W").removeClass("active");
+						});
+						
+						break;
+								
+			 		// user presses the "E" key STRAFE RIGHT
+					case 69:	$(".E").addClass("active");
+						$(document).keyup(function(e){
+						$(".E").removeClass("active");
+						});
+						
+						break;
+								
+					// user presses the "A" key LEFT 
+					case 65:	$(".A").addClass("active");
+						$(document).keyup(function(e){
+						$(".A").removeClass("active");
+						});
+
+						break;
+								
+					// user presses the "S" key REVERSE 
+					case 83:	$(".S").addClass("active");
+						$(document).keyup(function(e){
+						$(".S").removeClass("active");
+						});
+						
+						break; 
+
+					// user presses the "D" key RIGHT
+					case 68:	$(".D").addClass("active");
+						$(document).keyup(function(e){
+						$(".D").removeClass("active");
+						});
+						
+						break;
+
+								// user presses the "X" key STOP
+					case 88:	$(".X").addClass("active");
+						$(document).keyup(function(e){
+						$(".X").removeClass("active");
+						});
+						
+						break;
+				}
 	})
+});
 
-
+	// $('#robot').append().text(e.type + ':' + e.which);
 
 	// $(document).on('keypress', function(e){
 	// 	console.log('key pressed was ' + e.which);
@@ -14,13 +73,14 @@ $(document).ready (function() {
  //    //   }
 
 
-	});
+
 	
 
 
 
 
-
+// http://d2o0t5hpnwv4c1.cloudfront.net/013_keypress/demo/demo.html
+// http://net.tutsplus.com/tutorials/javascript-ajax/how-to-create-a-keypress-navigation-using-jquery/
 
 
 // });
